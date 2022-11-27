a52ad1acde0b53a4dd56fc1c06777c141c082173
Tushar Saini <s2sharpit@gmail.com>
2022-11-27 16:19:02 +0530

    added react-router-dom links

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,20 +1,29 @@
 import "./datatable.scss";
 import { DataGrid } from '@mui/x-data-grid';
 import { userColumns, userRows } from "../../data/datatablesource";
+import { Link } from "react-router-dom";
 
 export default function Datatable() {
 
-    const actionColumn = [{ field: "action", headerName: "Action", width: 200, renderCell: () => {
-        return (
-            <div className="cellAction">
-                <div className="viewButton">View</div>
-                <div className="deleteButton">Delete</div>
-            </div>
-        )
-    }}]
+    const actionColumn = [{
+        field: "action", headerName: "Action", width: 200, renderCell: () => {
+            return (
+                <div className="cellAction">
+                    <Link to="/users/test" style={{ textDecoration: "none" }}>
+                        <div className="viewButton">View</div>
+                    </Link>
+                    <div className="deleteButton">Delete</div>
+                </div>
+            )
+        }
+    }]
 
     return (
         <div className="datatable">
+            <div className="datatableTitle">
+                Add New User
+                <Link to="/users/new" style={{ textDecoration: "none" }} className="link">Add New </Link>
+            </div>
             <DataGrid
                 rows={userRows}
                 columns={userColumns.concat(actionColumn)}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,12 +11,15 @@ import PsychologyOutlinedIcon from '@mui/icons-material/PsychologyOutlined';
 import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import { Link } from "react-router-dom"
 
 export default function Sidebar() {
     return (
         <div className="sidebar">
             <div className="top">
+                <Link to="/" style={{ textDecoration: "none" }}>
                 <span className="logo">s2sharpit's admin</span>
+                </Link>
             </div>
             <hr />
             <div className="center">
@@ -27,14 +30,18 @@ export default function Sidebar() {
                         <span>Dashboard</span>
                     </li>
                     <p className="title">LISTS</p>
+                    <Link to="/users" style={{ textDecoration: "none" }}>
                     <li>
                         <PersonOutlineOutlinedIcon className="icon" />
                         <span>Users</span>
                     </li>
+                    </Link>
+                    <Link to="/products" style={{ textDecoration: "none" }}>
                     <li>
                         <StoreIcon className="icon" />
                         <span>Products</span>
                     </li>
+                    </Link>
                     <li>
                         <CreditCardIcon className="icon" />
                         <span>Orders</span>
