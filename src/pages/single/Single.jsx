00799e47009b1e788030c1cd267f240691a6712a
Tushar Saini <s2sharpit@gmail.com>
2022-11-27 15:08:19 +0530

    single item page design

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,5 +1,51 @@
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Chart from "../../components/chart/Chart"
+import Tables from "../../components/table/Table";
+
 export default function  Single() {
     return (
-        <div className="">Single</div>
+        <div className="single">
+            <Sidebar />
+            <div className="singleContainer">
+                <Navbar />
+                <div className="top">
+                    <div className="left">
+                        <div className="editButton">Edit</div>
+                        <h1 className="title">Information</h1>
+                        <div className="item">
+                            <img src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="" className="itemImg" />
+                            <div className="details">
+                                <h1 className="itemTitle">Ashneer Sai</h1>
+                                <div className="detailItem">
+                                    <span className="itemKey">Email:</span>
+                                    <span className="itemValue">ashneer@gmail.com</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Phone:</span>
+                                    <span className="itemValue">+91 9876543210</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Address:</span>
+                                    <span className="itemValue">Hyderabad, India</span>
+                                </div>
+                                <div className="detailItem">
+                                    <span className="itemKey">Country:</span>
+                                    <span className="itemValue">Bharat</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="right">
+                        <Chart aspect={3 / 1} title="User Spending (Last 6 Months)" />
+                    </div>
+                </div>
+                <div className="bottom">
+                    <h1 className="title">Last Transactions</h1>
+                    <Tables />
+                </div>
+            </div>
+        </div>
     )
 }
